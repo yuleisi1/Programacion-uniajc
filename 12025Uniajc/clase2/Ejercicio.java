f19d7a9658e24acf5b91953cbd619ca8fe23b48b
yuleisi1 <yscifuentes@estudiante.uniajc.edu.co>
2025-02-10 21:34:36 +0000

    progreso de la clase 2

---
+++ 12025Uniajc/clase2/Ejercicio.java
@@ -0,0 +1,13 @@
+public class Ejercicio {
+    public static void main(String[] args) {
+        // Area del cuadrado
+        // Declaracion de los variables
+        int lado, area;
+        // Asignamos un valor a la variable lado
+        lado = 3;
+        // Calculamos el area del cuadrado
+        area = lado * lado;
+        // Mostramos por pantalla la respuesta
+        System.out.println(" El area del cuadrado:" + area);
+    }
+}
